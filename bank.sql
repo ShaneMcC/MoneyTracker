269413e52d88150b245b6379c1ec02d1dbfe6eab
Shane Mc Cormack <shanemcc@gmail.com>
2014-04-30 05:05:30 +0100

    Update mysql schema.

--- bank.sql
+++ bank.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.13  Distrib 5.1.49, for debian-linux-gnu (i686)
+-- MySQL dump 10.13  Distrib 5.5.37, for debian-linux-gnu (x86_64)
 --
 -- Host: localhost    Database: bankinfo
 -- ------------------------------------------------------
--- Server version	5.1.49-1ubuntu8.1
+-- Server version	5.5.37-0ubuntu0.13.10.1
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -23,9 +23,9 @@ DROP TABLE IF EXISTS `accounts`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `accounts` (
-  `accountkey` varchar(20) NOT NULL,
+  `accountkey` varchar(34) NOT NULL,
   `sortcode` varchar(10) NOT NULL,
-  `accountnumber` varchar(10) NOT NULL,
+  `accountnumber` varchar(24) NOT NULL,
   `type` varchar(128) NOT NULL,
   `description` varchar(128) NOT NULL,
   `owner` varchar(128) NOT NULL,
@@ -40,15 +40,6 @@ CREATE TABLE `accounts` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `accounts`
---
-
-LOCK TABLES `accounts` WRITE;
-/*!40000 ALTER TABLE `accounts` DISABLE KEYS */;
-/*!40000 ALTER TABLE `accounts` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
 -- Table structure for table `transactions`
 --
 
@@ -58,12 +49,12 @@ DROP TABLE IF EXISTS `transactions`;
 CREATE TABLE `transactions` (
   `hash` varchar(100) NOT NULL,
   `time` int(11) NOT NULL,
-  `changetype` varchar(50) NOT NULL DEFAULT '',
-  `typecode` VARCHAR(40) NOT NULL,
+  `changetype` varchar(50) NOT NULL,
+  `typecode` varchar(40) NOT NULL,
   `description` varchar(512) NOT NULL DEFAULT '',
   `amount` double(10,2) NOT NULL,
   `balance` double(10,2) NOT NULL,
-  `accountkey` varchar(20) NOT NULL,
+  `accountkey` varchar(34) NOT NULL,
   `extra` text,
   `source` text,
   PRIMARY KEY (`hash`),
@@ -71,15 +62,6 @@ CREATE TABLE `transactions` (
   CONSTRAINT `transactions_ibfk_1` FOREIGN KEY (`accountkey`) REFERENCES `accounts` (`accountkey`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `transactions`
---
-
-LOCK TABLES `transactions` WRITE;
-/*!40000 ALTER TABLE `transactions` DISABLE KEYS */;
-/*!40000 ALTER TABLE `transactions` ENABLE KEYS */;
-UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -90,4 +72,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2011-02-02 17:29:45
+-- Dump completed on 2014-04-30  5:04:07
